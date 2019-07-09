502b145d920af617477aba41cdeb13f8956b07eb
ishanSengar <ishansengar435@gmail.com>
2019-07-10 02:47:23 +0530

    Update application.py

--- application.py
+++ application.py
@@ -1,6 +1,26 @@
-from flask import Flask
-app = Flask(__name__)
+# Import libraries
+import numpy as np
+from flask import Flask, request, jsonify
+from deeppavlov import build_model, configs
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+app = Flask(__name__)
+# Load the model
+model = build_model(configs.squad.squad, download=False)
+@app.route('/ques',methods=['POST'])
+def predict():
+    # Get the data from the POST request.
+    data = request.get_json(force=True)
+    # Make prediction using model loaded from disk as per the data.
+
+    print(data['passage'])
+    print(data['question'])
+    prediction = model([data['passage']],[data['question']])
+    answer={}
+    answer['result']=prediction[0]
+    answer['starting'] = prediction[1]
+    answer['logitvalue'] = prediction[2]
+    # Take the first value of prediction
+
+    return jsonify(answer)
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
